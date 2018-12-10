21d4c94d59cae307928f9f4846438eae0325896e
Ziyu Chen <461255138@qq.com>
2018-12-10 11:06:51 +0800

    first

---
+++ preprocess.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+
+
+class PreProcess(object):
+    '''preprocess pictures'''
+
+    def __init__(self, pic_name, flags=0):
+        self.pic_name = pic_name
+        self.img = cv2.imread(self.pic_name, flags)
+
+    def binarize(self, thresh, maxval, type):
+        #  ret_thresh1, self.img = cv2.threshold(self.img, thresh, maxval, type)
+        ret_thresh2, self.img = cv2.threshold(self.img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+    def show(self):
+        cv2.imshow("Current Picture", self.img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def morphEX(self, op=cv2.MORPH_CLOSE, kernel=np.ones((3, 3), np.uint8)):
+        self.img = cv2.morphologyEx(self.img, op=cv2.MORPH_OPEN, kernel=kernel)
+
+
+if __name__ == '__main__':
+    pp = PreProcess(pic_name='./data/t1.jpg', flags=0)
+    pp.show()
+    pp.binarize(100, 255, cv2.THRESH_BINARY)
+    pp.show()
+    pp.morphEX()
+    pp.morphEX()
+    pp.show()
+
+
+
