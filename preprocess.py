e6ea946f4eb839c60be5d45bcdacd496d84c4dd7
Ziyu Chen <461255138@qq.com>
2018-12-11 21:16:00 +0800

    second

--- preprocess.py
+++ preprocess.py
@@ -11,27 +11,40 @@ class PreProcess(object):
         self.copy = self.img.copy()
 
     def binarize(self, thresh, maxval, type):
-        #  ret_thresh1, self.img = cv2.threshold(self.img, thresh, maxval, type)
-        ret_thresh2, self.img = cv2.threshold(self.img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        # ret_thresh1, self.img = cv2.threshold(self.img, thresh, maxval, type)
+        ret_thresh2, self.img = cv2.threshold(self.img, 127, 255, cv2.THRESH_BINARY)
 
     def show(self):
         cv2.imshow("Current Picture", self.img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    def morphEX(self, op=cv2.MORPH_CLOSE, kernel=np.ones((3, 3), np.uint8)):
+    def morph_ex(self, op=cv2.MORPH_CLOSE, kernel=np.ones((3, 3), np.uint8)):
+        # kernel = np.array([[0, 1], [1, 1]], dtype=np.uint8)
         self.img = cv2.morphologyEx(self.img, op=cv2.MORPH_OPEN, kernel=kernel)
 
     def gauss_blur(self, size):
         self.img = cv2.blur(self.img, (size, size))
 
+    def lambda_binary(self, lamda):
+        self.img = pow(self.img/255, lamda)*255
 
+    def restore(self):
+        self.img = self.copy.copy()
+
+    def morph(self, erosion=True, kernel=np.array([[1, 1], [1, 1]], dtype=np.uint8)):
+        if erosion:
+            self.img = cv2.erode(self.img, kernel=kernel)
+        else:
+            self.img = cv2.dilate(self.img, kernel=kernel)
+
+8
 if __name__ == '__main__':
     pp = PreProcess(pic_name='./data/t1.jpg', flags=0)
     pp.show()
-    pp.binarize(100, 255, cv2.THRESH_BINARY)
-    pp.show()
-    pp.img.dilated
+    # pp.binarize(100, 255, cv2.THRESH_BINARY)
+    # pp.binarize(100, 255, cv2.THRESH_BINARY)
+    pp.lambda_binary(0.001)
     pp.show()
 
 
--- preprocess_gui.py
+++ preprocess_gui.py
@@ -14,24 +14,36 @@ class PreProcessBoard(PreProcess):
     def __init__(self, pic_name, flags=0):
         PreProcess.__init__(self, pic_name, flags)
 
-        cv2.namedWindow(winname=PreProcessBoard.winname, flags=0)
-        cv2.resizeWindow(PreProcessBoard.winname, (int(self.img.shape[1]/2), int(self.img.shape[0]/2)))
+        cv2.namedWindow(winname=PreProcessBoard.winname, flags=1)
+        h, w = self.img.shape
+        print(self.img.shape)
+        # cv2.resizeWindow(PreProcessBoard.winname, int(w/2), int(h/2))
         cv2.createTrackbar('kernel size', PreProcessBoard.winname, 0, 5, nothing)
         cv2.createTrackbar('bin thresh', PreProcessBoard.winname, 127, 255, nothing)
+        cv2.createTrackbar('lambda', PreProcessBoard.winname, 0, 10000, nothing)
 
         while 1:
             s = cv2.getTrackbarPos('kernel size', PreProcessBoard.winname)
             thresh = cv2.getTrackbarPos('bin thresh', PreProcessBoard.winname)
             kernel = np.ones((s, s), np.uint8)
-            img_mor = cv2.morphologyEx(self.img, op=cv2.MORPH_CLOSE, kernel=kernel)
-            ret, img_bin = cv2.threshold(self.img, thresh, 255, cv2.THRESH_BINARY)
+            lamda = cv2.getTrackbarPos('lambda', PreProcessBoard.winname)
+
+            # img_mor = cv2.morphologyEx(self.img, op=cv2.MORPH_CLOSE, kernel=kernel)
+            # ret, img_bin = cv2.threshold(self.img, thresh, 255, cv2.THRESH_BINARY)
+            self.restore()
+            self.lambda_binary(lamda/100.0)
+            self.binarize(thresh, 255, type=cv2.THRESH_BINARY)
+            self.img = 255 - self.img
+            # self.gauss_blur(2)
+            self.morph(True)
+            self.img = 255 - self.img
             # self.binarize(thresh, 255, cv2.THRESH_BINARY_INV)
             # cv2.imshow(PreProcessBoard.winname, img_mor)
             key = cv2.waitKey(1)
             if key == 27:
                 break
             else:
-                cv2.imshow(PreProcessBoard.winname, img_bin)
+                cv2.imshow(PreProcessBoard.winname, self.img)
 
 
 if __name__ == '__main__':
